25f5c363162ed3e5f00e3625e3e3efed7979d33c
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2021-12-24 13:43:12 +0530

    Bank SM

--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -14,6 +14,12 @@ contract dBank {
 
     //odd events
     event Deposit(address indexed _user, uint256 _amount, uint256 _timestamp);
+    event Withdraw(
+        address indexed _user,
+        uint256 _userBalance,
+        uint256 _depositTime,
+        uint256 intrest
+    );
 
     constructor(Token _token) {
         token = _token;
@@ -34,6 +40,8 @@ contract dBank {
         //take the deposited ammount and add it to the existing balance
         etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
         //adding up the timestamp value from the current block.
+        //beacuse the user gets to deposit money only once the timestamps wont overlap
+        //adding because default value for uint in solidity is 0
         depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
         //setting the deposit status to true
         isDeposited[msg.sender] = true;
@@ -41,7 +49,34 @@ contract dBank {
         emit Deposit(msg.sender, msg.value, block.timestamp);
     }
 
-    function withdraw() public {}
+    function withdraw() public {
+        //checks
+        require(
+            isDeposited[msg.sender] == true,
+            "You have to deposit to withdraw"
+        );
+        uint256 userBalance = etherBalanceOf[msg.sender];
+
+        //the total time the user is holding his coins in the bank is the below expression
+        uint256 depositTime = block.timestamp - depositStart[msg.sender];
+        //cal the intrest a user earned by the time of his/her withdrawing
+        uint256 intrestPerSecond = 31668017 *
+            (etherBalanceOf[msg.sender] / 1e16);
+        uint256 intrest = intrestPerSecond * depositTime;
+
+        //withdraw the ethers in the account completely and trasfer it to the depositor
+        msg.sender.transfer(userBalance);
+        etherBalanceOf[msg.sender] = 0;
+        //minting the tokens that are earned as intrest by the user
+        token.mint(msg.sender, intrest);
+
+        //resetting the users data
+        depositStart[msg.sender] = 0;
+        isDeposited[msg.sender] = false;
+
+        //emit the event
+        emit Withdraw(msg.sender, userBalance, depositTime, intrest);
+    }
 
     function borrow() public payable {}
 
