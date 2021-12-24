cd34458d15d1137b6eac9c3b5e9513f96bc759e2
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2021-12-24 13:15:04 +0530

    Deposit Crypto tests and Code!

--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -6,11 +6,40 @@ import "./Token.sol";
 contract dBank {
     Token private token;
 
+    //mapping for keeping the track of addresses and there balance of eth
+
+    mapping(address => uint256) public etherBalanceOf;
+    mapping(address => uint256) public depositStart;
+    mapping(address => bool) public isDeposited;
+
+    //odd events
+    event Deposit(address indexed _user, uint256 _amount, uint256 _timestamp);
+
     constructor(Token _token) {
         token = _token;
     }
 
-    function deposit() public payable {}
+    //deposits the ethers into the bank
+    function deposit() public payable {
+        //if the user has already deposited then revert the transaction
+        require(
+            isDeposited[msg.sender] == false,
+            "Your Deposit is Already active"
+        );
+        require(
+            msg.value >= 1e16,
+            "Deposit must be of greater then or equal 0.01 ETH"
+        );
+
+        //take the deposited ammount and add it to the existing balance
+        etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
+        //adding up the timestamp value from the current block.
+        depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+        //setting the deposit status to true
+        isDeposited[msg.sender] = true;
+        //emitting the event
+        emit Deposit(msg.sender, msg.value, block.timestamp);
+    }
 
     function withdraw() public {}
 
